3549389b82a2c2ce74e1facbaeaf2ce233253c05
Paul Water <p.r.water@setbased.nl>
2018-07-18 11:31:36 +0200

    Added class NotPreferredUrlException.

---
+++ src/NotPreferredUrlException.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SetBased\Abc\Exception;
+
+/**
+ * Exception for situations where the requested URL is not valid.
+ */
+class NotPreferredUrlException extends \RuntimeException
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * The preferred URI.
+   *
+   * @var string
+   */
+  public $preferredUri;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * NotPreferredUrlException constructor.
+   *
+   * @param string $preferredUri The preferred URI.
+   */
+  public function __construct(string $preferredUri)
+  {
+    parent::__construct('Not preferred URL');
+
+    $this->preferredUri = $preferredUri;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
